eeb70f76ab9d403b1bb9ad16c743711d78bc05fc
Peng.Zhao <654882654@163.com>
2018-12-06 18:17:06 +0800

    实现商品分类的树形结构

---
+++ ly-item/ly-item-service/src/main/java/com/leyou/item/service/CategoryService.java
@@ -0,0 +1,29 @@
+package com.leyou.item.service;
+
+import com.leyou.common.enums.ExceptionEnum;
+import com.leyou.common.exception.LyException;
+import com.leyou.item.mapper.CategoryMapper;
+import com.leyou.item.pojo.Category;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+    @Autowired
+    private CategoryMapper categoryMapper;
+
+    public List<Category> queryCategoryListByPid(Long pid) {
+        // 通过实体对象中非空的值作为查询条件
+        Category t = new Category();
+        t.setParentId(pid);
+        List<Category> list = categoryMapper.select(t);
+        // 判断查询结果是否为空
+        if (CollectionUtils.isEmpty(list)) {
+            throw new LyException(ExceptionEnum.CATEGORY_NOT_FOUND);
+        }
+        return list;
+    }
+}
