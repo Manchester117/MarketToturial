635c18d6c5c492ee6b560406212253906cbc2dc5
zhao.peng <zhao.peng@zhaopin.com.cn>
2019-02-23 18:53:22 +0800

    实现图片上传

--- ly-upload/src/main/java/com/leyou/upload/service/UploadService.java
+++ ly-upload/src/main/java/com/leyou/upload/service/UploadService.java
@@ -1,8 +1,14 @@
 package com.leyou.upload.service;
 
+import com.github.tobato.fastdfs.domain.StorePath;
+import com.github.tobato.fastdfs.service.FastFileStorageClient;
 import com.leyou.common.enums.ExceptionEnum;
 import com.leyou.common.exception.LyException;
+import com.leyou.upload.config.UploadProperties;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -15,14 +21,20 @@ import java.util.List;
 
 @Slf4j
 @Service
+@EnableConfigurationProperties(UploadProperties.class)
 public class UploadService {
-    private static final List<String> ALLOW_TYPES = Arrays.asList("image/jpeg", "image/jpg", "image/png", "image/bmp");
+    @Autowired
+    private FastFileStorageClient storageClient;
+    @Autowired
+    private UploadProperties props;
+
+//    private static final List<String> ALLOW_TYPES = Arrays.asList("image/jpeg", "image/jpg", "image/png", "image/bmp");
 
     public String uploadImage(MultipartFile file) {
         try {
             // 校验文件类型
             String contentType = file.getContentType();
-            if (!ALLOW_TYPES.contains(contentType)) {
+            if (!props.getAllowTypes().contains(contentType)) {
                 throw new LyException(ExceptionEnum.INVALID_FILE_TYPE);
             }
             // 校验文件内容
@@ -31,10 +43,18 @@ public class UploadService {
                 throw new LyException(ExceptionEnum.INVALID_FILE_TYPE);
             }
             // 保存文件到本地
-            File destination = new File("D:\\JavaWebWorkSpace\\leyou\\upload", file.getOriginalFilename());
-            file.transferTo(destination);
+//            File destination = new File("D:\\JavaWebWorkSpace\\leyou\\upload", file.getOriginalFilename());
+//            file.transferTo(destination);
             // 返回路径
-            return "http://image.leyou.com/" + file.getOriginalFilename();
+//            return "http://image.leyou.com/" + file.getOriginalFilename();
+
+            // 保存到FastDFS
+            // 获取文件后缀名
+            String extension = StringUtils.substringAfterLast(file.getOriginalFilename(), ".");
+            // 执行上传
+            StorePath storePath = this.storageClient.uploadFile(file.getInputStream(), file.getSize(), extension, null);
+            // 返回文件路径
+            return props.getBaseUrl() + storePath.getFullPath();
         } catch (IOException e) {
             // 上传失败
             log.error("上传文件失败", e);
